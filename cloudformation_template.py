6fbc60b64d39d1fc075d57a9d7aa80671ceab4d9
Traun Leyden <tleyden@couchbase.com>
2015-04-09 11:03:57 -0700

    add sync gw and gateload

--- cloudformation_template.py
+++ cloudformation_template.py
@@ -13,6 +13,63 @@ t.add_description(
     'An Ec2-classic stack with Couchbase Server, Sync Gateway + load testing tools '
 )
 
+NUM_COUCHBASE_SERVERS=3
+NUM_SYNC_GW_SERVERS=3
+NUM_GATELOADS=3
+
+def createCouchbaseSecurityGroups(t):
+
+    # Couchbase security group
+    secGrpCouchbase = ec2.SecurityGroup('CouchbaseSecurityGroup')
+    secGrpCouchbase.GroupDescription = "Allow access to Couchbase Server"
+    secGrpCouchbase.SecurityGroupIngress = [
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="22",
+            ToPort="22",
+            CidrIp="0.0.0.0/0",
+        ),
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="8091",
+            ToPort="8091",
+            CidrIp="0.0.0.0/0",
+        ),
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="4984",
+            ToPort="4984",
+            CidrIp="0.0.0.0/0",
+        )
+    ]
+
+    # Add security group to template
+    t.add_resource(secGrpCouchbase)
+
+    cbIngressPorts = [
+        {"FromPort": "4369", "ToPort": "4369" },    # couchbase server
+        {"FromPort": "4985", "ToPort": "4985" },    # sync gw admin 
+        {"FromPort": "9876", "ToPort": "9876" },    # gateload
+        {"FromPort": "5984", "ToPort": "5984" },    # couchbase server
+        {"FromPort": "8092", "ToPort": "8092" },    # couchbase server 
+        {"FromPort": "11210", "ToPort": "11210" },  # couchbase server
+        {"FromPort": "11211", "ToPort": "11211" },  # couchbase server
+        {"FromPort": "21100", "ToPort": "21299" },  # couchbase server
+    ]
+
+    for cbIngressPort in cbIngressPorts:
+        from_port = cbIngressPort["FromPort"]
+        to_port = cbIngressPort["ToPort"]
+        name = 'CouchbaseSecurityGroupIngress{}'.format(from_port)
+        secGrpCbIngress = ec2.SecurityGroupIngress(name)
+        secGrpCbIngress.GroupName = Ref(secGrpCouchbase)
+        secGrpCbIngress.IpProtocol = "tcp"
+        secGrpCbIngress.FromPort = from_port
+        secGrpCbIngress.ToPort = to_port
+        secGrpCbIngress.SourceSecurityGroupName = Ref(secGrpCouchbase)
+        t.add_resource(secGrpCbIngress)
+
+
 #
 # Parameters
 #
@@ -21,49 +78,10 @@ keyname_param = t.add_parameter(Parameter(
     Description='Name of an existing EC2 KeyPair to enable SSH access'
 ))
 
-# Create a security group
-secGrpCouchbase = ec2.SecurityGroup('CouchbaseSecurityGroup')
-secGrpCouchbase.GroupDescription = "Allow access to Couchbase Server"
-secGrpCouchbase.SecurityGroupIngress = [
-    ec2.SecurityGroupRule(
-        IpProtocol="tcp",
-        FromPort="22",
-        ToPort="22",
-        CidrIp="0.0.0.0/0",
-    ),
-    ec2.SecurityGroupRule(
-        IpProtocol="tcp",
-        FromPort="8091",
-        ToPort="8091",
-        CidrIp="0.0.0.0/0",
-    )
-]
-
-# Add security group to template
-t.add_resource(secGrpCouchbase)
-
-cbIngressPorts = [
-    {"FromPort": "4369", "ToPort": "4369" },
-    {"FromPort": "5984", "ToPort": "5984" },
-    {"FromPort": "11210", "ToPort": "11210" },
-    {"FromPort": "11211", "ToPort": "11211" },
-    {"FromPort": "21100", "ToPort": "21299" },
-]
-
-for cbIngressPort in cbIngressPorts:
-    from_port = cbIngressPort["FromPort"]
-    to_port = cbIngressPort["ToPort"]
-    name = 'CouchbaseSecurityGroupIngress{}'.format(from_port)
-    secGrpCbIngress = ec2.SecurityGroupIngress(name)
-    secGrpCbIngress.GroupName = Ref(secGrpCouchbase)
-    secGrpCbIngress.IpProtocol = "tcp"
-    secGrpCbIngress.FromPort = from_port
-    secGrpCbIngress.ToPort = to_port
-    secGrpCbIngress.SourceSecurityGroupName = Ref(secGrpCouchbase)
-    t.add_resource(secGrpCbIngress)
+secGrpCouchbase = createCouchbaseSecurityGroups(t)
 
 # Couchbase Server Instances
-for i in xrange(3):
+for i in xrange(NUM_COUCHBASE_SERVERS):
     name = "couchbaseserver{}".format(i)
     instance = ec2.Instance(name)
     instance.ImageId = "ami-403b4328"
@@ -73,6 +91,28 @@ for i in xrange(3):
     instance.Tags=Tags(Name=name)
     t.add_resource(instance)
 
+# Sync Gw instances (ubuntu ami)
+for i in xrange(NUM_SYNC_GW_SERVERS):
+    name = "syncgateway{}".format(i)
+    instance = ec2.Instance(name)
+    instance.ImageId = "ami-96a818fe"  # centos7 
+    instance.InstanceType = "m1.large"
+    instance.SecurityGroups = [Ref(secGrpCouchbase)]
+    instance.KeyName = Ref(keyname_param)
+    instance.Tags=Tags(Name=name)
+    t.add_resource(instance)
+
+# Gateload instances (ubuntu ami)
+for i in xrange(NUM_GATELOADS):
+    name = "gateload{}".format(i)
+    instance = ec2.Instance(name)
+    instance.ImageId = "ami-96a818fe"  # centos7 
+    instance.InstanceType = "m1.large"
+    instance.SecurityGroups = [Ref(secGrpCouchbase)]
+    instance.KeyName = Ref(keyname_param)
+    instance.Tags=Tags(Name=name)
+    t.add_resource(instance)
 
 print(t.to_json())
+
 
