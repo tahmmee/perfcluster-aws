e8ade7c5652c1313796b411ab0b63af5ce01aaca
Ubuntu <ubuntu@ip-172-31-2-136.ec2.internal>
2015-11-11 15:27:16 +0000

    parameterize s3_region

--- aws.sh
+++ aws.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-file="$1"
-
-key_id="$3"
-key_secret="$4"
-path="$1"
-bucket="$2"
+s3_region="$1"
+key_id="$4"
+key_secret="$5"
+file="$2"
+bucket="$3"
 content_type="application/octet-stream"
 date="$(LC_ALL=C date -u +"%a, %d %b %Y %X %z")"
 md5="$(openssl md5 -binary < "$file" | base64)"
 
-sig="$(printf "PUT\n$md5\n$content_type\n$date\n/$bucket/$path" | openssl sha1 -binary -hmac "$key_secret" | base64)"
+sig="$(printf "PUT\n$md5\n$content_type\n$date\n/$bucket/$file" | openssl sha1 -binary -hmac "$key_secret" | base64)"
 
-curl -T $file http://$bucket.s3-eu-west-1.amazonaws.com/$path \
+curl -T $file http://$bucket.s3-$s3_region.amazonaws.com/$file \
     -H "Date: $date" \
     -H "Authorization: AWS $key_id:$sig" \
     -H "Content-Type: $content_type" \
